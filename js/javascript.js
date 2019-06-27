76cb5d5e5195c86a16219adf328604f93cda09a2
Gian Roberto zoli <gianrobertozoli@gmail.com>
2019-06-27 08:23:24 +0200

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,26 +1,53 @@
-// var slideIndex = 1;
-// const slides = document.getElementsByClassName("mySlides");
-// showSlides(slideIndex);
+//slider
 
-// Next/previous controls
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+var right = 0;
+var maxMargin;
+var jumpMargin = 150;
+
+function setWidth(){
+  var boxwidth = document.querySelector(".container").offsetWidth;
+  var displaywidth = document.querySelector(".row").offsetWidth;
+  var displayheight = document.querySelector(".row").offsetHeight;
+  var children = document.querySelectorAll(".row-container > .container").length;
+  var outerboxwidth = children * boxwidth + (children*10);
+  document.querySelector(".row-container").style.width = outerboxwidth+"px";
+  document.querySelectorAll("button")[0].style.height = displayheight+"px";
+  document.querySelectorAll("button")[1].style.height = displayheight+"px";
+  maxMargin = outerboxwidth - displaywidth;
 }
 
-function showSlides(n) {
-  var i;
+(function(){
+  
+    var listEl = document.querySelector('.home-grid.products-grid.products-grid--max-4');
+  var btnLeftEl = document.querySelector('#left-btn');
+  var btnRightEl = document.querySelector('#right-btn');
+  var count = 0;
 
-
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
+  function slideImages(dir){
+    var totalChildren = listEl.querySelectorAll(".item").length;
+    dir === "left" ? ++count : --count;
+    listEl.style.left = count * 286 + 'px';
+    btnLeftEl.style.display = count < 0 ? "block" : "none";
+    // Here, 4 is the number displayed at any given time
+    btnRightEl.style.display = count > 4-totalChildren ? "block" : "none";
   }
 
-  slides[slideIndex-1].style.display = "block"; 
-}
+  btnLeftEl.addEventListener("click", function(e) {
+      slideImages("left");
+  });
+  btnRightEl.addEventListener("click", function(e) {
+      slideImages("right");
+  });
+
+  
+})();
+
+
+
+
+
+
 
 // Arrow
 
@@ -35,22 +62,8 @@ document.addEventListener('keydown', function(event) {
 });
 
 
-// Automatic slideShow
 
-var slideIndex = 0;
-showSlides();
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none"; 
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1} 
-  slides[slideIndex-1].style.display = "block"; 
-  setTimeout(showSlides, 5000); // Change image every 5 seconds
-}
 
 
 
