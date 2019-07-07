8323b92879c536080c6bb91351567c1f05ae7e4a
Gian Roberto zoli <gianrobertozoli@gmail.com>
2019-07-07 16:36:36 +0200

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,47 +1,47 @@
 //slider
 
 
-var right = 0;
-var maxMargin;
-var jumpMargin = 150;
+// var right = 0;
+// var maxMargin;
+// var jumpMargin = 150;
 
-function setWidth(){
-  var boxwidth = document.querySelector(".container").offsetWidth;
-  var displaywidth = document.querySelector(".row").offsetWidth;
-  var displayheight = document.querySelector(".row").offsetHeight;
-  var children = document.querySelectorAll(".row-container > .container").length;
-  var outerboxwidth = children * boxwidth + (children*10);
-  document.querySelector(".row-container").style.width = outerboxwidth+"px";
-  document.querySelectorAll("button")[0].style.height = displayheight+"px";
-  document.querySelectorAll("button")[1].style.height = displayheight+"px";
-  maxMargin = outerboxwidth - displaywidth;
-}
+// function setWidth(){
+//   var boxwidth = document.querySelector(".container").offsetWidth;
+//   var displaywidth = document.querySelector(".row").offsetWidth;
+//   var displayheight = document.querySelector(".row").offsetHeight;
+//   var children = document.querySelectorAll(".row-container > .container").length;
+//   var outerboxwidth = children * boxwidth + (children*10);
+//   document.querySelector(".row-container").style.width = outerboxwidth+"px";
+//   document.querySelectorAll("button")[0].style.height = displayheight+"px";
+//   document.querySelectorAll("button")[1].style.height = displayheight+"px";
+//   maxMargin = outerboxwidth - displaywidth;
+// }
 
-(function(){
+// (function(){
   
-    var listEl = document.querySelector('.home-grid.products-grid.products-grid--max-4');
-  var btnLeftEl = document.querySelector('#left-btn');
-  var btnRightEl = document.querySelector('#right-btn');
-  var count = 0;
+//     var listEl = document.querySelector('.home-grid.products-grid.products-grid--max-4');
+//   var btnLeftEl = document.querySelector('#left-btn');
+//   var btnRightEl = document.querySelector('#right-btn');
+//   var count = 0;
 
-  function slideImages(dir){
-    var totalChildren = listEl.querySelectorAll(".item").length;
-    dir === "left" ? ++count : --count;
-    listEl.style.left = count * 286 + 'px';
-    btnLeftEl.style.display = count < 0 ? "block" : "none";
-    // Here, 4 is the number displayed at any given time
-    btnRightEl.style.display = count > 4-totalChildren ? "block" : "none";
-  }
+//   function slideImages(dir){
+//     var totalChildren = listEl.querySelectorAll(".item").length;
+//     dir === "left" ? ++count : --count;
+//     listEl.style.left = count * 286 + 'px';
+//     btnLeftEl.style.display = count < 0 ? "block" : "none";
+//     // Here, 4 is the number displayed at any given time
+//     btnRightEl.style.display = count > 4-totalChildren ? "block" : "none";
+//   }
 
-  btnLeftEl.addEventListener("click", function(e) {
-      slideImages("left");
-  });
-  btnRightEl.addEventListener("click", function(e) {
-      slideImages("right");
-  });
+//   btnLeftEl.addEventListener("click", function(e) {
+//       slideImages("left");
+//   });
+//   btnRightEl.addEventListener("click", function(e) {
+//       slideImages("right");
+//   });
 
   
-})();
+// })();
 
 
 
